ed7af503de4d39ae41db0cf457a411f25c878444
Jo_sue_yeon <yoy07030@khu.ac.kr>
2021-01-12 02:27:59 +0900

    api 추가

--- App.js
+++ App.js
@@ -6,3 +6,19 @@ import Mainloading from "./loading";
 export default function App() {
   return <Mainloading />;
 }
+
+var xhr = new XMLHttpRequest();
+var url = 'http://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo'; /*URL*/
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+''; /*Service Key*/
+queryParams += '&' + encodeURIComponent('solYear') + '=' + encodeURIComponent('2017'); /**/
+queryParams += '&' + encodeURIComponent('solMonth') + '=' + encodeURIComponent('09'); /**/
+xhr.open('GET', url + queryParams);
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      const data='Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText;
+
+        console.log(data);
+    }
+};
+
+xhr.send('');
